1d93be4708e789da45fe933f9906accf0f87066f
kjza178 <kira178@gmail.com>
2025-08-02 01:32:34 +0700

    Update app_simple.py

--- app_simple.py
+++ app_simple.py
@@ -700,6 +700,10 @@ def check_multi_status():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
 
+@app.route("/")
+def index():
+    return "✅ App is running"
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False) 
